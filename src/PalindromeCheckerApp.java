bc70203f04cd1bc12378d3f150607bf7d05d4844
dhwanisharmaa <dhwanisharma2206@gmail.com>
2026-02-26 15:33:49 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,44 @@
 public class PalindromeCheckerApp {
-    public static void main (String[] args){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialised successfully.");
+    public static void main(String[] args){
+       System.out.println("Welcome to the Palindrome Checker Management System");
+       System.out.println("Version: 1.0");
+       System.out.println("System initialised successfully.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to check palindrome: ");
+        String input = scanner.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Enqueue and Push characters
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue vs pop
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+
+        scanner.close();
+
 
     }
+
 }
