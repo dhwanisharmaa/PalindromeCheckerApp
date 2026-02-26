72a609b9ba60e6e5b30acce6527efbf259d10595
dhwanisharmaa <dhwanisharma2206@gmail.com>
2026-02-26 16:08:54 +0530

    UC6: Queue and Stack

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,32 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-       System.out.println("Welcome to the Palindrome Checker Management System");
-       System.out.println("Version: 1.0");
-       System.out.println("System initialised successfully.");
-
-        String word = "radar";
+        String word = "noon";
         System.out.println("Checking word: " + word);
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+        for (int i = 0; i < word.length(); i++)
+        {
+            char ch = word.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+        boolean isPalindrome = true;
+        while (!queue.isEmpty() && !stack.isEmpty())
+        {
+            char fromQueue = queue.remove();
+            char fromStack = stack.pop();
+            if (fromQueue != fromStack)
+            {
+                isPalindrome = false;
+                break;
+            }
         }
-        if (word.equals(reversed)) {
+        if (isPalindrome)
+        {
             System.out.println("Result: \"" + word + "\" is a palindrome.");
-        } else {
+        }
+        else
+        {
             System.out.println("Result: \"" + word + "\" is NOT a palindrome.");
         }
         System.out.println("Program finished.");
