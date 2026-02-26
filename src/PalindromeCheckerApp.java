1d21b6d48d0788d9efbde66b6a51aca8c450bb1d
dhwanisharmaa <dhwanisharma2206@gmail.com>
2026-02-26 14:56:55 +0530

    Updated Develop Branch

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@ public class PalindromeCheckerApp {
     public static void main (String[] args){
         System.out.println("Welcome to Palindrome Checker Management System");
         System.out.println("Version: 1.0");
-        System.out.println("Ssytem initialised succefully.");
+        System.out.println("System initialised successfully.");
 
     }
 }
